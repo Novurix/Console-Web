29953a91229507689c5978a2031f405e3393035f
Dmitri <56200546+Novurix@users.noreply.github.com>
2020-05-21 09:49:35 -0400

    Commands.cs
    this holds the commands

---
+++ Commands.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace WebConsole {
+  
+      public class Commands {
+            public string[] cmds =
+            {
+                " ",
+                "commands - shows a list of commands",
+                "web <URL> - goes to a website with <URL>",
+                " ",
+                "html getId <ID> - finds an element with the id of <ID>",
+                "html getClass <CLASS_NAME> - finds an element with the class of <CLASS_NAME>",
+                "html getName <NAME> - finds an element with the class of <NAME>",
+                "html remove - removes the selected element",
+                "html elemCl - deletes the selected element from the HTML",
+                "html text <TEXT> - sets text of a selected element",
+                "html properties - finds the properties of an element",
+                " "
+            };
+      }
+}
