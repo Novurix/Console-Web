04e6d403dabb34a47dbea5b25c23cdd2b58cca7a
Dmitri <56200546+Novurix@users.noreply.github.com>
2020-05-21 09:47:29 -0400

    Update ConsoleWeb.cs

--- ConsoleWeb.cs
+++ ConsoleWeb.cs
@@ -1,202 +1,234 @@
-﻿using System;
+using System;
 using System.Threading;
 
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace web
+namespace WebConsole
 {
     public class ConsoleWeb
     {
         static IWebDriver driver;
-        static IWebElement element;
-
-        static string input;
-        static string webPage;
-
-        static string id;
-        static bool isID;
+        static IWebElement webElement;
 
         static Commands commands;
+        static string input;
+        static string id;
+
+        static bool isId, isClass, isName;
 
         public static void Main(string[] args)
         {
+            driver = new Driver();
             commands = new Commands();
 
-            ChromeDriver chromeDriver = new ChromeDriver();
-            chromeDriver.NetworkConditions = new ChromeNetworkConditions()
-            { DownloadThroughput = 0, UploadThroughput = 0, Latency = TimeSpan.FromMilliseconds(1), IsOffline=false};
-
-            driver = chromeDriver;
-
-
-            Console.WriteLine(" ");
-            for (int i = 0; i < commands.commands.Length; i++)
+            for (int i = 0; i < commands.cmds.Length; i++)
             {
-                Console.WriteLine(commands.commands[i]);
+                Console.WriteLine(commands.cmds[i]);
             }
-            Console.WriteLine(" ");
-            
+
             getInput();
         }
 
+
         static void getInput()
         {
-            Console.ForegroundColor = ConsoleColor.White;
             input = Console.ReadLine();
-            init();
+            scanInput();
         }
 
-        static void init()
+        static void scanInput()
         {
             string[] words = input.Split();
 
-            if (words.Length == 2 && element == null)
-            {
-                // FINDING A WEBSITE
-
-                if (words[0].ToLower() == "web" && words[1].StartsWith("https://"))
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    driver.Url = words[1];
-                }
-                else if (words[0].ToLower() == "web" && !words[1].StartsWith("https://"))
-                {
-                    string url = "https://"+words[1];
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    driver.Url = url;
-                }
-            }
-            else if (words.Length == 2 && element != null)
-            {
-                // Checking if a selected element exists to prevent moving to another site
-
-                string getToLower = words[0].ToLower();
-                if (getToLower == "web" && words[1].StartsWith("https://"))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("you've assigned an element to " + driver.Url + " to continue, type 'elem delete'");
-                }
-
-                // Deleting selected element
-
-                if (getToLower == "elem" && words[1].ToLower() == "delete")
-                {
-                    element = null;
-                }
-
-                // Clearing input
-
-                else if (getToLower == "elem" && words[1].ToLower() == "tclear")
-                {
-                    if (element != null)
-                    {
-                        try {
-                            element.Clear();
-                        }
-                        catch (Exception e) { Console.WriteLine(e.GetBaseException()); }
-                    }
-                }
-
-                // Clicking a button
-
-                else if (words[0].ToLower() == "elem" && words[1].ToLower() == "click")
-                {
-                    try {
-                        element.Click();
-                    }
-                    catch(Exception e){}
-                }
-            }
-
-            if (words.Length == 3)
-            {
-                // FINDING ELEMENTS
-
-                if (words[0].ToLower() == "findweb" && words[1].ToLower() == "id")
-                {
-                    element = driver.FindElement(By.Id(words[2]));
-                    isID = true; 
-                }
-
-                if (words[0].ToLower() == "findweb" && words[1].ToLower() == "class")
-                {
-                    element = driver.FindElement(By.ClassName(words[2]));
-                    isID = false;
-                    id = words[2];
-                }
-
-                // Changing text of an element
-
-                else if (words[0].ToLower() == "elem" && words[1].ToLower() == "text")
-                {
-                    if (element != null)
-                    {
-                        IJavaScriptExecutor js = driver as IJavaScriptExecutor;
-                        //element.SendKeys(words[2]);
-                        if (isID)
-                        {
-                            try
-                            {
-                                js.ExecuteScript("document.getElementById('" + id + "').textContent = '" + words[2] + "';");
-                            }
-                            catch (Exception e)
-                            {
-                                try {
-                                    element.SendKeys(words[2]);
-                                } catch (Exception e1) {}
-                            }
-                        }
-                        else
-                        {
-
-                            try {
-                                js.ExecuteScript("document.querySelector('" + id + "').textContent = '" + words[2] + "';");
-                            }
-                            catch (Exception e) {
-                                try {
-                                    element.SendKeys(words[2]);
-                                } catch (Exception e1) {}
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("no element has been assigned");
-                    }
-                }
-            }
-
-            else if (words.Length == 1)
+            if (words.Length == 1)
             {
                 if (words[0].ToLower() == "commands")
                 {
-                    Console.WriteLine(" ");
-                    for (int i = 0; i < commands.commands.Length; i++)
+                    for (int i = 0; i < commands.cmds.Length; i++)
                     {
-                        Console.WriteLine(commands.commands[i]);
+                        Console.WriteLine(commands.cmds[i]);
                         Thread.Sleep(20);
                     }
-                    Console.WriteLine(" ");
                 }
+            }
+
+            else if (words.Length == 2) {
+
+                if (words[0].ToLower() == "web") {
+                    string http = "https://";
+
+                    if (words[1].StartsWith(http)) {
+                        driver.Url = words[1];
+                    }
+                    else {
+                        driver.Url = "https://" + words[1];
+                    }
+                }
+
+                if (words[0].ToLower() == "html")
+                {
+                    if (words[1].ToLower() == "remove")
+                    {
+                        webElement = null;
+                    }
+
+                    else if (words[1].ToLower() == "elemcl")
+                    {
+                        IJavaScriptExecutor javaScriptExecutor = driver as IJavaScriptExecutor;
+                        if (isId) {
+                            if (webElement != null) {
+                                webElement = null;
+                                javaScriptExecutor.ExecuteScript("var id = '" + id + "'; " +
+                                    "var element = document.getElementById(id); " +
+                                    "element.parentNode.removeChild(element)");
+                            }
+                            else {
+                                Console.WriteLine("the ID: " + id + " does not exist and current action cannot proceed");
+                            }
+                        }
+                        else if (isClass) {
+                            if (webElement != null) {
+                                webElement = null;
+                                javaScriptExecutor.ExecuteScript(
+                                    "var id = '." + id + "'; " +
+                                    "var element = document.querySelector(id);" +
+                                    "element.parentNode.removeChild(element)");
+                            }
+                            else {
+                                Console.WriteLine("the CLASS_NAME: " + id + " does not exist and current action cannot proceed");
+                            }
+                        }
+                        else if (isName) {
+                            if (webElement != null) {
+                                webElement = null;
+                                javaScriptExecutor.ExecuteScript(
+                                    "var id = '." + id + "'; " +
+                                    " var element = document.querySelector(id);" +
+                                    " element.parentNode.removeChild(element)");
+                            }
+                            else {
+                                Console.WriteLine("the NAME: " + id + " does not exist and current action cannot proceed");
+                            }
+                        }
+                    }
+                }
+            }
+            else if (words.Length == 3)
+            {
+                if (words[0].ToLower() == "html")
+                {
+                    if (words[1].ToLower() == "getid") {
+                        try {
+                            webElement = driver.FindElement(By.Id(words[2]));
+                            id = words[2];
+                            isId = true; isClass = false; isName = false;
+                        }
+                        catch(Exception e) {
+                            Console.WriteLine("cannot find element with ID: " + words[2]);
+                        }
+                    }
+                    else if (words[1].ToLower() == "getclass") {
+                        try {
+                            webElement = driver.FindElement(By.ClassName(words[2]));
+                            id = words[2];
+                            isId = false; isClass = true; isName = false;
+                        }
+                        catch (Exception e) {
+                            Console.WriteLine("cannot find element with CLASS_NAME: " + words[2]);
+                        }
+                    }
+                    else if (words[1].ToLower() == "getname") {
+                        try {
+                            webElement = driver.FindElement(By.Name(words[2]));
+                            id = words[2];
+                            isId = false; isClass = false; isName = true;
+                        }
+                        catch(Exception e) {
+                            Console.WriteLine("cannot find element with NAME: " + words[2]);
+                        }
+                    }
+
+                    else if (words[1].ToLower() == "text"){
+                        if (webElement != null) {
+                            try {
+                                webElement.Clear();
+                                webElement.SendKeys(words[2]);
+                            } catch(Exception e) {
+                                try {
+                                    IJavaScriptExecutor javaScriptExecutor = driver as IJavaScriptExecutor;
+                                    if (isId) {
+                                        javaScriptExecutor.ExecuteScript("var id = '" + id + "'; " +
+                                            "var element = document.getElementById(id); " +
+                                            "element.textContent = '" + words[2] + "'");
+                                    }
+                                    else if (isClass) {
+                                        javaScriptExecutor.ExecuteScript("var id = '." + id + "'; " +
+                                            "var element = document.querySelector(id); " +
+                                            "element.textContent = '" + words[2] + "'");
+                                    }
+                                    else if (isName) {
+                                        javaScriptExecutor.ExecuteScript("var id = '." + id + "'; " +
+                                            "var element = document.querySelector(id); " +
+                                            "element.textContent = '" + words[2] + "'");
+                                    }
+
+                                }catch(Exception e1) {}
+                            }
+                        }
+                        else {
+                            Console.WriteLine("cannot set text of null");
+                        }
+                    }
+                }
+            }
+            else if (words.Length > 3)
+            {
+                string text = "";
+                if (words[0].ToLower() == "html" && words[1].ToLower() == "text") {
+                    for (int i = 0; i < words.Length-2; i++) {
+                        text = text + words[i+2] + " ";
+
+                        if (webElement != null) {
+                            try {
+                                webElement.SendKeys(text);
+                            } catch(Exception e) {
+                                try  {
+                                    IJavaScriptExecutor javaScriptExecutor = driver as IJavaScriptExecutor;
+                                    if (isId)  {
+                                        javaScriptExecutor.ExecuteScript("var id = '" + id + "'; " +
+                                            "var element = document.getElementById(id); " +
+                                            "element.textContent = '" + text + "'");
+                                    }
+                                    else if (isClass) {
+                                        javaScriptExecutor.ExecuteScript("var id = '." + id + "'; " +
+                                            "var element = document.querySelector(id); " +
+                                            "element.textContent = '" + text + "'");
+                                    }
+                                    else if (isName) {
+                                        javaScriptExecutor.ExecuteScript("var id = '." + id + "'; " +
+                                            "var element = document.querySelector(id); " +
+                                            "element.textContent = '" + text + "'");
+                                    }
+                                }
+                                catch (Exception e1){}
+                            }
+                        }
+                    }
+                }
+                text = "";
             }
 
             getInput();
         }
     }
 
-    class Commands
+    class Driver : ChromeDriver
     {
-        public string[] commands =
+        public Driver()
         {
-            "commands - shows list of commands",
-            "web <URL> - goes to url",
-            "findweb id <ID> - gets an element by it's <ID> in the website",
-            "findweb class <CLASS_NAME> - gets an element by it's <CLASS_NAME> in the website",
-            "elem delete - removes the focus of a selected element",
-            "elem text <TEXT> - sets the text of a selected element",
-            "elem tclear - clears the text of a selected element",
-            "elem click - clicks the selected element"
-        };
+            this.NetworkConditions = new ChromeNetworkConditions()
+            { DownloadThroughput = 0, UploadThroughput = 0, Latency = TimeSpan.FromMilliseconds(1), IsOffline = false };
+        }
     }
 }
